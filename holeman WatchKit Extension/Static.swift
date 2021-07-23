0957876c41917173ebf350f5c9673159fcf9eadd
nubeble <jdub.kim@gmail.com>
2021-07-23 23:32:33 +0900

    1.0.5 (10)

--- holeman WatchKit Extension/Static.swift
+++ holeman WatchKit Extension/Static.swift
@@ -27,15 +27,15 @@ struct Static {
      */
     
     // ToDo: debug (현재 좌표 - 골프장 좌표)
-    // (골드그린)
+    // (골드그린 from 매탄동)
     // static let __lat = 1.753586614270796
     // static let __lon = -2.033034733589
     
-    // (신안)
+    // (신안 from 매탄동)
     // static let __lat = 0.16496653122842
     // static let __lon = -0.20835852071767
     
-    // (이천실크밸리)
+    // (이천실크밸리 from 매탄동)
     // static let __lat = 0.18166362940429
     // static let __lon = -0.53033352453059
     
@@ -43,9 +43,13 @@ struct Static {
     // static let __lat = 2.148933074033644
     // static let __lon = -1.78062070655674
     
+    // (용원 from 부산 부곡동)
+    static let __lat = 0.12429597950647
+    static let __lon = 0.26976349408446
+    
     // (에머슨 from 매탄동)
-    static let __lat = 0.323004315571045
-    static let __lon = -0.29435637277493
+    // static let __lat = 0.323004315571045
+    // static let __lon = -0.29435637277493
     
     // (골드그린 from 정관)
     // static let __lat = -0.18721253449693
